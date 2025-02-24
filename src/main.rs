316963308d85783cab3eecc1799a7c88e134f808
Mike Doran <mikrdoran@gmail.com>
2025-02-24 11:01:54 +0000

    .

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use infinite_debate::modules::entities;
 use infinite_debate::modules::game_management;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (player1_hand, remaining_cards) = game_management::get_intial_deck().unwrap();
+    let (player1_hand, mut remaining_cards) = game_management::get_intial_deck().unwrap();
     println!("{:?}\n", player1_hand);
     println!("{:?}\n", remaining_cards);
+    let next_cards = remaining_cards.draw_new_cards(2).unwrap();
+    println!("next: {:?}\n", next_cards);
     let actions = entities::get_actions()?;
     for a in actions {
         println!("{:?}\n", a)
